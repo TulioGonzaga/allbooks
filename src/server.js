f9c7b50e7fabfa33276eb0639fbca65ba5b18798
Tulio <tulio.gonzaga@yahoo.com.br>
2024-06-02 19:39:38 -0300

    Altera o server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log ("Boas vindas a API ao Allbooks")
+  console.log("API disponível atraves da url em http://localhost:8000")
 })
